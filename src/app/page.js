35f6da531b38835b286d9c104c4caf1f70ae730b
Sonu Thakur <sonu88ggn@gmail.com>
2025-07-03 00:35:19 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+// import DigitalServicesLanding from '@/components/DigitalServicesLanding'
+import DigitalServicesLanding from "@/components/Home";
+export default function Home() {
+  return (
+    <>
+     <DigitalServicesLanding />
+    </>
+  );
+}
